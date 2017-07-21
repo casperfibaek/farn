34081226287a718bc7e0341cfcada85769a83267
Casper Fibæk <casperfibaek@gmail.com>
2017-07-21 14:23:37 +0200

    pointFile

---
+++ index.js
@@ -0,0 +1,4 @@
+const farn = require('./lib/farn');
+
+console.log(farn.intersect([[0, 0, 0], [2, 2, 0]], [[1, 2, 0], [3, 1, 0]]));
+console.log(farn.lineStringLength([[0, 0, 0], [2, 2, 0]]));
--- init.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable */
-const base = require('./lib/base');
-const bufferLine = require('./lib/buffer');
