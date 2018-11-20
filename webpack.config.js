91ebd9d6fb0c8dd5c238cbb673e82c50089d0fe0
Casper Samsø Fibæk (CFI) <CFI@NIRAS.DK>
2018-11-20 11:42:51 +0000

    web build

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './lib/farn.js',
+  output: {
+    filename: 'farn.js',
+    path: path.resolve(__dirname, 'out/'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
