a6a5825b1b363055b144472852762f2b5f587154
Casper Samsø Fibæk (CFI) <CFI@NIRAS.DK>
2018-11-20 11:05:34 +0000

    test, bench

--- index.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-const farn = require('./lib/farn');
-
-console.log(farn.intersect([[0, 0, 0], [2, 2, 0]], [[1, 2, 0], [3, 1, 0]]));
-console.log(farn.lineStringLength([[0, 0, 0], [2, 2, 0]]));
